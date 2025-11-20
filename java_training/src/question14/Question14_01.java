e69de89610237be5ad7fc09a69fb71e2f0fe7a74
user <user@WIN-ORFJ1NHAE1E>
2025-11-20 18:17:23 +0900

    11/20 朝の確認問題 day5 q01,02

---
+++ java_training/src/question14/Question14_01.java
@@ -0,0 +1,25 @@
+package question14;
+
+// クラス作成
+public class Question14_01 {
+	// フィールド
+	private int num;
+	private double gas;
+
+	// メソッドを定義
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public void setGas(double gas) {
+		this.gas = gas;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public double getGas() {
+		return gas;
+	}
+}
